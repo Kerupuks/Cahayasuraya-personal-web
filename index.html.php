0633995fa8c54b4419f72f3a384d00589c9e74b5
CahayaSurya <septianirawan07@gmail.com>
2024-12-01 21:05:10 +0700

    Add files via upload

---
+++ index.html.php
@@ -0,0 +1,325 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Toko Cahaya Surya</title>
+
+        <!-- CSS FILES -->        
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&family=Open+Sans&display=swap" rel="stylesheet">
+                        
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="css/bootstrap-icons.css" rel="stylesheet">
+
+        <link href="css/templatemo-topic-listing.css" rel="stylesheet">      
+
+    </head>
+    
+    <body id="top">
+
+        <main>
+
+            <nav class="navbar navbar-expand-lg">
+                <div class="container">
+                    <a class="navbar-brand" href="index.php">
+                        <i class="bi-back"></i>
+                        <span>Cahaya Surya</span>
+                    </a>
+
+                    <div class="d-lg-none ms-auto me-4">
+                        <a href="#top" class="navbar-icon bi-person smoothscroll"></a>
+                    </div>
+    
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+    
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav ms-lg-5 me-lg-auto">
+                            <li class="nav-item">
+                                <a class="nav-link click-scroll" href="#section_1">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link click-scroll" href="#section_2">Sejarah Toko</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link click-scroll" href="#section_3">Daftar Brand</a>
+                            </li>
+    
+                            <li class="nav-item">
+                                <a class="nav-link click-scroll" href="#section_4">Contact</a>
+                            </li>
+                        </ul>
+
+                        <!-- <div class="d-none d-lg-block">
+                            <a href="#top" class="navbar-icon bi-person smoothscroll"></a>
+                        </div> -->
+                    </div>
+                </div>
+            </nav>
+            
+            <section class="hero-section d-flex justify-content-center align-items-center" id="section_1">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-8 col-12 mx-auto">
+                <h1 class="text-white text-center">Selamat Datang di Toko Listrik Cahaya Surya</h1>
+
+                <h6 class="text-center">Penyedia Perlengkapan Listrik Terpercaya di Pekanbaru</h6>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+
+            <section class="faq-section section-padding" id="section_2">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-lg-6 col-12">
+                            <h2 class="mb-4">Sejarah Toko</h2>
+                        </div>
+
+                        <div class="clearfix"></div>
+
+                        <div class="col-lg-5 col-12">
+                            <img src="images/5088.jpg" class="img-fluid" alt="FAQs" style="border-radius: 15px;">
+                        </div>
+
+
+                        <div class="col-lg-6 col-12 m-auto">
+                            <div class="accordion" id="accordionExample">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="headingOne">
+                                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                        Sejarah Berdirinya Toko Listrik Cahaya Surya
+                                        </button>
+                                    </h2>
+
+                                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body" style="text-align: justify; text-align-last: center; line-height: 1.6;">
+                                        <p style="margin-bottom: 20px; text-indent: 30px; line-height: 1.6;">
+                                            Toko Listrik Cahaya Surya didirikan di kota Pekanbaru pada tanggal <strong>18 Maret 2024</strong>. yang berlamat di jl.Sepakat, Kelurahan Mentangor, Kecamatan Kulim. 
+                                            Sebagai salah satu toko perlengkapan listrik terpercaya, kami berkomitmen untuk menyediakan berbagai kebutuhan listrik dengan kualitas terbaik. 
+                                            Produk yang tersedia mencakup beragam bola lampu, terminal listrik, kabel berbagai jenis, sakelar, panel listrik, dan alat-alat lainnya untuk memenuhi kebutuhan rumah tangga, perkantoran, maupun proyek skala besar.
+                                        </p>
+                                        <p style="margin-bottom: 20px; text-indent: 30px; line-height: 1.6;">
+                                            Dengan layanan yang ramah dan profesional, kami selalu berusaha memberikan pengalaman belanja yang nyaman bagi setiap pelanggan. 
+                                            Toko Listrik Cahaya Surya terus berkembang dengan memperluas koleksi produk dan meningkatkan pelayanan, sehingga menjadi pilihan utama masyarakat Pekanbaru dan sekitarnya dalam memenuhi kebutuhan perlengkapan listrik.
+                                        </p>
+
+                                    </div>
+                                </div>
+
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="headingTwo">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        Perubahan Transisi Toko 
+                                    </button>
+                                    </h2>
+
+                                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                                    
+                                    <div class="accordion-body" style="text-align: justify;">
+                                        <p style="text-indent: 30px;">Sebelum perubahan transisi toko yang sekarang, toko ini dulunya merupakan toko pecah belah yang berdiri sejak <strong>tahun 2004</strong>. Toko ini berlokasi di Jalan Kartini, Kecamatan Sungai Apit, Kabupaten Siak, Provinsi Riau. Seiring berjalannya waktu, kami menyadari bahwa kebutuhan akan perlengkapan listrik semakin meningkat, baik untuk kebutuhan rumah tangga, perkantoran, maupun industri. Oleh karena itu, kami memutuskan untuk bertransformasi dan memperluas produk yang kami tawarkan, menjadikannya toko perlengkapan listrik yang lengkap dan terpercaya.</p>
+
+                                        <p style="text-indent: 30px;">Perubahan transisi ini dilakukan dengan penuh pertimbangan dan strategi, mengingat adanya peluang besar untuk bersaing di pasar lokal dengan kualitas dan layanan terbaik. Kami berkomitmen untuk tidak hanya menyediakan produk berkualitas, tetapi juga memberikan pelayanan yang ramah dan profesional kepada setiap pelanggan. Kini, dengan pengalaman lebih dari 20 tahun dalam bisnis ini, Toko Listrik Cahaya Surya telah menjadi pilihan utama masyarakat Pekanbaru dan sekitarnya dalam memenuhi berbagai kebutuhan perlengkapan listrik. Kami terus berinovasi, menjaga kualitas produk, dan berupaya memberikan solusi terbaik untuk setiap pelanggan kami.</p>
+                                    </div>
+                                </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+            <section class="brand-section py-5" id="section_3">
+                <div class="container">
+                <div class="row">
+    <div class="col-12 text-center">
+        <h2 class="mb-4">Daftar Merek Terpercaya Yang Bisa Anda Temukan di Toko Kami</h2>
+    </div>
+    <div class="col-12">
+        <div id="logoCarousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+            <div class="carousel-inner">
+                <!-- Brand 1 -->
+                <div class="carousel-item active">
+                    <img src="images/Hannochs.jpg" alt="Hannoch" class="d-block mx-auto" style="max-width: 150px; height: auto;">
+                </div>
+                <!-- Brand 2 -->
+                <div class="carousel-item">
+                    <img src="images/Philips.jpg" alt="Philips" class="d-block mx-auto" style="max-width: 150px; height: auto;">
+                </div>
+                <!-- Brand 3 -->
+                <div class="carousel-item">
+                    <img src="images/Megaman.jpg" alt="Megaman" class="d-block mx-auto" style="max-width: 150px; height: auto;">
+                </div>
+                <!-- Brand 4 -->
+                <div class="carousel-item">
+                    <img src="images/Miyako.jpg" alt="Miyako" class="d-block mx-auto" style="max-width: 150px; height: auto;">
+                </div>
+                <!-- Brand 5 -->
+                <div class="carousel-item">
+                    <img src="images/Rinnai.jpg" alt="Rinnai" class="d-block mx-auto" style="max-width: 150px; height: auto;">
+                </div>
+                <!-- Brand 6 -->
+                <div class="carousel-item">
+                    <img src="images/Schneider.jpg" alt="Schneider" class="d-block mx-auto" style="max-width: 150px; height: auto;">
+                </div>
+                <!-- Brand 7 -->
+                <div class="carousel-item">
+                    <img src="images/Advance.jpg" alt="Advance" class="d-block mx-auto" style="max-width: 150px; height: auto;">
+                </div>
+                <!-- Brand 8 -->
+                <div class="carousel-item">
+                    <img src="images/Maspion.jpg" alt="Maspion" class="d-block mx-auto" style="max-width: 150px; height: auto;">
+                </div>
+                <!-- Brand 9 -->
+                <div class="carousel-item">
+                    <img src="images/Uticon.jpg" alt="Uticon" class="d-block mx-auto" style="max-width: 150px; height: auto;">
+                </div>
+            </div>
+            <!-- Controls (optional) -->
+            <button class="carousel-control-prev" type="button" data-bs-target="#logoCarousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#logoCarousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+</div>
+
+                </div>
+             </section>
+
+            <section class="contact-section section-padding section-bg" id="section_4">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-12 col-12 text-center">
+                <h2 class="mb-5">Lokasi Toko </h2>
+            </div>
+
+            <div class="col-lg-5 col-12 mb-4 mb-lg-0">
+                <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3979.376924360062!2d101.50817121455041!3d0.47942069999999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31d5af3310ecd5c5%3A0x7e5dfe91a59a2cb7!2sToko%20Listrik%20Cahaya%20Surya!5e0!3m2!1sid!2sid!4v1696528172000!5m2!1sid!2sid"
+                width="100%" 
+                height="250" 
+                style="border:0;" 
+                allowfullscreen="" 
+                loading="lazy" 
+                referrerpolicy="no-referrer-when-downgrade">
+                </iframe>
+            </div>
+            <div class="col-lg-6 col-md-6 col-12 mb-3 mb-lg-0 text-start">
+                <h4 class="mb-3">Lokasi Toko</h4>
+                <p>Jl.Sepakat, Kec.Kulim, Kota-Pekanbaru, Prov.Riau, Indonesia</p>
+                <hr>
+                <p class="d-flex align-items-center mb-1">
+                    <span class="me-2">No Telephone</span>
+                    <a href="https://wa.me/+6282173010298" class="site-footer-link">082173010298</a>
+                </p>
+                <p class="d-flex align-items-center">
+                    <span class="me-2">Email</span>
+                    <a href="mailto:cahayasuryaListrik@gmail.com" class="site-footer-link">aatiam883@gmail.com</a>
+                </p>
+            </div>
+
+
+        </div>
+    </div>
+</section>
+
+        </main>
+
+<footer class="site-footer section-padding">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-3 col-10 mb-4 pb-2">
+                        <a class="navbar-brand mb-2" href="index.php">
+                            <i class="bi-back"></i>
+                            <span>Cahaya Surya</span>
+                        </a>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <h6 class="site-footer-title mb-3">Beranda</h6>
+
+                        <ul class="site-footer-links">
+                            <li class="site-footer-link-item">
+                                <a href="#" class="site-footer-link">Home</a>
+                            </li>
+
+                            <li class="site-footer-link-item">
+                                <a href="#" class="site-footer-link">Sejarah Toko</a>
+                            </li>
+
+                            <li class="site-footer-link-item">
+                                <a href="#" class="site-footer-link">Brand</a>
+                            </li>
+
+                            <li class="site-footer-link-item">
+                                <a href="#" class="site-footer-link">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-6 mb-4 mb-lg-0">
+                        <h6 class="site-footer-title mb-3">Informasi</h6>
+
+                        <p class="text-white d-flex mb-1">
+                            <a href="https://wa.me/+6282173010298" class="site-footer-link" target="_blank">
+                                082173010298
+                            </a>
+                        </p>
+
+                        <p class="text-white d-flex">
+                            <a href="mailto:info@company.com" class="site-footer-link">
+                                aatiam883@gmail.com
+                            </a>
+                        </p>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-12 mt-4 mt-lg-0 ms-auto">
+                        <div >
+                            <button class="btn btn-secondary" type="button"  aria-expanded="false">
+                            Indonesia</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <p class="copyright-text mt-lg-5 mt-4" style="text-align: center;">
+    &copy; 2024 <strong>Toko Listrik Cahaya Surya</strong>. Hak cipta dilindungi. 
+</p>
+
+        </footer>
+
+
+        <!-- JAVASCRIPT FILES -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/jquery.sticky.js"></script>
+        <script src="js/click-scroll.js"></script>
+        <script src="js/custom.js"></script>
+
+    </body>
+</html>
